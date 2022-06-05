e5da358662ae8803ba1f6acf0780eb243dd84291
programmerkoji <60305092+programmerkoji@users.noreply.github.com>
2022-06-05 10:22:07 +0900

    seederとshow.blade.phpを修正

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -1,34 +1,67 @@
 <x-guest-layout>
-    <style>
-        h1 {
-            text-align: center;
-            padding: 30px;
-        }
-        .container {
-            width: 80%;
-            margin: 0 auto;
-        }
-        table {
-            border-spacing: 0;
-            border-collapse: collapse;
-            border-bottom: 1px solid #aaa;
-            color: #555;
-            width: 100%;
-        }
-        th {
-            border-top: 1px solid #aaa;
-            background-color: #f5f5f5;
-            padding: 10px 0 10px 6px;
-            text-align: center;
-        }
-        td {
-            border-top: 1px solid #aaa;
-            padding: 10px 0 10px 6px;
-            text-align: center;
-        }
-        a {
-            margin-right: 20px;
-        }
-    </style>
-    <h1>タスク詳細</h1>
-    </x-guest-layout>
+<style>
+    h1 {
+        text-align: center;
+        padding: 30px;
+    }
+    .container {
+        width: 60%;
+        margin: 0 auto;
+    }
+    table {
+        width: 100%;
+        border-collapse: collapse;
+        border-spacing: 0;
+    }
+
+    table th,table td {
+        padding: 10px 0;
+        text-align: center;
+    }
+
+    table tr:nth-child(odd){
+        background-color: #eee
+    }
+    .link {
+        display: flex;
+        justify-content: space-between;
+        margin-top: 16px;
+    }
+</style>
+<h1>タスク詳細</h1>
+<div class="container">
+    <table>
+        <tr>
+            <th>ID</th>
+            <td>{{ $task->id }}</td>
+        </tr>
+        <tr>
+            <th>タスク</th>
+            <td>{{ $task->name }}</td>
+        </tr>
+        <tr>
+            <th>タスク内容</th>
+            <td>{{ $task->content }}</td>
+        </tr>
+        <tr>
+            <th>作成日時</th>
+            <td>{{ $task->created_at->format('Y年m月d日 H:i') }}</td>
+        </tr>
+        <tr>
+            <th>更新日時</th>
+            <td>{{ $task->updated_at->format('Y年m月d日 H:i') }}</td>
+        </tr>
+    </table>
+    <div class="link">
+        <div class="link__back">
+            <a href="/">戻る</a>
+        </div>
+        <div class="link__edit">
+            <a href="">編集する</a>
+        </div>
+        <div class="link__delete">
+            <a href="">削除する</a>
+        </div>
+    </div>
+</div>
+</x-guest-layout>
